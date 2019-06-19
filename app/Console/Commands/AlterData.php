ef221aa34ef9e4c9d8a99b846d49aeb11455891b
Martien Dermawan Tanama <martiendt@gmail.com>
2019-06-19 14:45:13 +0700

    seed groups

--- app/Console/Commands/AlterData.php
+++ app/Console/Commands/AlterData.php
@@ -2,7 +2,7 @@
 
 namespace App\Console\Commands;
 
-use App\Model\Master\Item;
+use App\Model\Master\Group;
 use App\Model\Project\Project;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\DB;
@@ -47,23 +47,24 @@ class AlterData extends Command
             config()->set('database.connections.tenant.database', env('DB_DATABASE').'_'.strtolower($project->code));
             DB::connection('tenant')->reconnect();
 
-            $items = [
-                'B001 REGULER BUBUK 250GR',
-                'B005 REGULER KOPI GULA 20GR',
-                'B008 REGULER CUP HOREKA 1KG',
-                'B011 REGULER 3 IN 1 BULK 1KG',
-                'R001 PREMIUM PACK 70GR',
-                'R002 PREMIUM PACK 5GR',
-                'R003 PREMIUM CUP KOPI GULA',
-                'R008 PREMIUM BIJI SEAL PACK 1KG',
-                'R012 PREMIUM PACK HOREKA 1KG',
-                'Y007 NEW GEN BULK PACK 1KG',
+            $groups = [
+                'Hotel',
+                'Resto',
+                'Cafe',
+                'Toko',
+                'Warung',
+                'Agen',
+                'Grosir',
+                'Mini Market',
             ];
 
-            for ($i = 0; $i < count($items); $i++) {
-                $item = Item::where('name', $items[$i])->first();
-                $item->code = explode(' ', $items[$i])[0];
-                $item->save();
+            for ($i = 0; $i < count($groups); $i++) {
+                if (!Group::where('name', $groups[$i])->first()) {
+                    $group = new Group;
+                    $group->name = $groups[$i];
+                    $group->class_reference = 'Customer';
+                    $group->save();
+                }
             }
         }
     }
